5e9f6d75bf1ed47eb6846b835c1a0560098e268e
resolvaaquiapp <hdmicro.ml@gmail.com>
2026-07-01 06:12:48 -0300

    Update Google Apps Script URL in api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 // Substitua o link abaixo pela sua URL real do Google (ela deve ficar exatamente aqui)
-const URL_APPS_SCRIPT = "https://script.google.com/macros/library/d/13SbD4K1VDkaFSCMMdSU8rDYcmkIe9WeJtzl--U-yWmt-rhXTiDLlZI8w/23";
+const URL_APPS_SCRIPT = "https://script.google.com/macros/library/d/13SbD4K1VDkaFSCMMdSU8rDYcmkIe9WeJtzl--U-yWmt-rhXTiDLlZI8w/24";
 
 function salvarConsultaNoBackend(dadosUsuario, estado) {
   // Se a URL estiver vazia ou não configurada, interrompe
